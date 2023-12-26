2f71df434b8b9b15a257016633d23aa0ca5a3dfe
Sophia Sorensen <sophiarosesorensen@gmail.com>
2023-12-26 11:10:30 -0800

    Linked table up to app

--- world-project/src/App.js
+++ world-project/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Navbar } from "./Navbar";
+import { Table } from "./Table";
 
 function App() {
     return (
         <div className="global-margin">
             <Router>
                 <Navbar />
+                <Table />
             </Router>
         </div>
     );
