f50bae022c08daa54fed1515acdd430d08ff0410
Sophia Sorensen <sophiarosesorensen@gmail.com>
2023-12-27 16:37:37 -0800

    Routers set up

---
+++ src/ErrorPage.js
@@ -0,0 +1,5 @@
+export function ErrorPage() {
+    return (
+      <div>Oops, an error occurred. Working on it!</div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {ErrorPage} from "./ErrorPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        errorElement: <ErrorPage/>,
+    }
+])
+
+root.render(
+    <RouterProvider router={ router }/>
+);
